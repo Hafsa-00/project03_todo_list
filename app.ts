3e2ebe802df24200d46746714ecdb41694bf137d
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-05-10 10:15:41 +0500

    1

---
+++ app.ts
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+const Todos: string[] = [];
+
+async function manageTodos() {
+    while (true) {
+        const action = await inquirer.prompt([
+            {
+                name: 'action',
+                type: 'list',
+                message: 'Choose an action:',
+                choices: ['Add', 'Delete', 'Replace', 'Read', 'Exit']
+            }
+        ]);
+
+        switch (action.action) {
+            case 'Add':
+                const addTask = await inquirer.prompt([
+                    {
+                        name: 'todo',
+                        type: 'input',
+                        message: 'What would you like to add to your todos?'
+                    }
+                ]);
+                Todos.push(addTask.todo);
+                break;
+
+            case 'Delete':
+                const deleteIndex = await inquirer.prompt([
+                    {
+                        name: 'index',
+                        type: 'number',
+                        message: 'Enter the index of the task you want to delete:'
+                    }
+                ]);
+                if (deleteIndex.index >= 0 && deleteIndex.index < Todos.length) {
+                    Todos.splice(deleteIndex.index, 1);
+                } else {
+                    console.log('Invalid index. Task not deleted.');
+                }
+                break;
+
+            case 'Replace':
+                const replaceIndex = await inquirer.prompt([
+                    {
+                        name: 'index',
+                        type: 'number',
+                        message: 'Enter the index of the task you want to replace:'
+                    }
+                ]);
+                if (replaceIndex.index >= 0 && replaceIndex.index < Todos.length) {
+                    const replaceTask = await inquirer.prompt([
+                        {
+                            name: 'todo',
+                            type: 'input',
+                            message: 'Enter the new task:'
+                        }
+                    ]);
+                    Todos[replaceIndex.index] = replaceTask.todo;
+                } else {
+                    console.log('Invalid index. Task not replaced.');
+                }
+                break;
+
+            case 'Read':
+                console.log('Your todos:', Todos);
+                break;
+
+            case 'Exit':
+                console.log('Exiting...');
+                return;
+
+            default:
+                console.log('Invalid action. Please choose a valid action.');
+        }
+    }
+}
+
+manageTodos();
---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+let Todos = [];
+let condition = true
+while(condition)
+
+{let addtask = await inquirer.prompt(
+[{
+    name: 'todo',
+    type: 'input',
+    message: 'what would you like to add in your todos?'
+},
+{
+    name:'addmore',
+    type:'confirm',
+    message:'Do you want to add more?',
+    default:'false'
+}
+]
+);
+Todos.push(addtask.todo);
+condition:addtask.addmore;
+console.log(Todos);}
